fbfdb7bfd5d27bca5c215e201e5363bbec6c41d7
jcoop32 <jcooper11@hawk.iit.edu>
2023-06-21 08:49:01 -0500

    init

---
+++ models/car.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const carSchema = new mongoose.Schema(
+  {
+    make: { type: String, required: true },
+    model: String,
+    year: {
+      type: Number,
+      min: 1886,
+      max: {
+        function() {
+          return new Date().getFullYear() + 1;
+        },
+      },
+    },
+    color: String,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model('Car', carSchema);
